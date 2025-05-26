3938a23ecd68e5c4d094b8b14b2106b2faf1a5c6
Dan Gocan <dangocan@outlook.com>
2025-05-26 16:55:17 +0100

    Auto deployment

--- thought.py
+++ thought.py
@@ -1,4 +1,10 @@
 # TODO
 
 # Need to make sure the name of the blog is alphanumeric
-# A
+# QR Code embedding for your own blog 
+# A simple newsletter integration, where users can create their own mailing list 
+# A post can pe part X of a TOTAL (like part of a book, or series)
+# Monthly writing prompts and competitions 
+# Profile badges and titles 
+# #1 poster per previous week gets a spotlight 
+# Lupta cu globalismul scriind o poezie. 
